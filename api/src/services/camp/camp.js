7630430ac5f7ff942720630758d17bed576d9e26
nampdn <nampdn@protonmail.com>
2020-08-29 12:27:08 +0700

    fix: 🐛 remove google lib

--- api/src/services/camp/camp.js
+++ api/src/services/camp/camp.js
@@ -1,14 +1,5 @@
 import { db } from 'src/lib/db'
 
-import { PhoneNumberUtil, PhoneNumberFormat } from 'google-libphonenumber'
-
-export const formatPhoneNumber = (phoneNumber) => {
-  const phoneUtil = PhoneNumberUtil.getInstance()
-  const number = phoneUtil.parseAndKeepRawInput(phoneNumber, 'VN')
-  const formattedNumber = phoneUtil.format(number, PhoneNumberFormat.E164)
-  return formattedNumber
-}
-
 export const campRegister = async ({ input }) => {
   if (input.meta) {
     const meta = JSON.parse(input.meta)
