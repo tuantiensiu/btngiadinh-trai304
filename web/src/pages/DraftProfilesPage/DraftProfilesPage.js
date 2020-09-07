044edc669d477297f5955c1ace1e39e69c6b45d5
nampdn <nampdn@protonmail.com>
2020-09-08 06:29:38 +0700

    fix: 🐛 import typo

--- web/src/pages/DraftProfilesPage/DraftProfilesPage.js
+++ web/src/pages/DraftProfilesPage/DraftProfilesPage.js
@@ -1,6 +1,6 @@
 import DraftProfilesLayout from 'src/layouts/DraftProfilesLayout'
 import DraftProfilesCell from 'src/components/DraftProfilesCell'
-import SMSBalanceCell from 'src/components/SMSBalanceCell'
+import SMSBalanceCell from 'src/components/SmsBalanceCell'
 
 const DraftProfilesPage = () => {
   return (
