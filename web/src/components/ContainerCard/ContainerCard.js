1d0b970cce73014232c73c13518a085b1256d78a
nampdn <nampdn@protonmail.com>
2020-09-25 17:53:39 +0700

    feat: 🎸 able to attach profile to a container

--- web/src/components/ContainerCard/ContainerCard.js
+++ web/src/components/ContainerCard/ContainerCard.js
@@ -1,10 +1,65 @@
-import { Link, routes } from '@redwoodjs/router'
+import { useState, useRef } from 'react'
+import { useMutation, useFlash } from '@redwoodjs/web'
+import { Link, navigate, routes } from '@redwoodjs/router'
 import ProfileListItem from 'src/components/ProfileListItem'
+import ProfileSelectCell from 'src/components/ProfileSelectCell'
 
+const ATTACH_PROFILES_TO_CONTAINER_MUTATION = gql`
+  mutation ATTACH_PROFILES_TO_CONTAINER(
+    $containerId: String!
+    $profileIds: [String!]!
+  ) {
+    attachProfilesToContainer(
+      containerId: $containerId
+      profileIds: $profileIds
+    )
+  }
+`
+
+const DETACH_PROFILE_FROM_CONTAINER_MUTATION = gql`
+  mutation DETACH_PROFILE_TO_CONTAINER(
+    $containerId: String!
+    $profileId: String!
+  ) {
+    detachProfileFromContainer(containerId: $containerId, profileId: $profileId)
+  }
+`
 const ContainerCard = ({ data }) => {
-  const { id, name, type, note, capacity, profiles } = data
+  const [enableInput, setEnableInput] = useState(false)
+  const selectedProfiles = useRef([])
+  const { id, name, note, capacity, profiles } = data
+  const [attachProfilesToContainer, { loading }] = useMutation(
+    ATTACH_PROFILES_TO_CONTAINER_MUTATION,
+    {
+      onCompleted: () => {
+        navigate(routes.containers())
+      },
+    }
+  )
+
+  const toggleInput = () => {
+    setEnableInput(!enableInput)
+  }
+
+  const attachProfile = () => {
+    console.log(`Attaching profiles...`)
+    attachProfilesToContainer({
+      variables: {
+        containerId: id,
+        profileIds: selectedProfiles.current,
+      },
+    })
+  }
+
+  const onSelectionChange = (profiles) => {
+    selectedProfiles.current = profiles.map((p) => p.id)
+    console.log(selectedProfiles.current)
+  }
+
+  if (loading) return 'Loading...'
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+    <div className="flex flex-col max-w-sm rounded shadow-lg">
       <div className="px-6 py-4 bg-green-500 text-white">
         <div className="flex flex-row justify-between">
           <div className="font-bold text-xl mb-2">{name}</div>
@@ -15,11 +70,29 @@ const ContainerCard = ({ data }) => {
         <p className="text-gray-400 text-base">{note}</p>
       </div>
       <ul>
-        {profiles.map(({ profile }, index) => (
-          <ProfileListItem key={profile.id} profile={profile} index={index} />
-        ))}
+        {profiles.length > 0 ? (
+          profiles.map(({ profile }, index) => (
+            <ProfileListItem key={profile.id} profile={profile} index={index} />
+          ))
+        ) : (
+          <li className="p-4">
+            Không gian trống, bấm nút Gán để xếp người vào đây
+          </li>
+        )}
       </ul>
-      <div className="px-6 pt-4 pb-2">
+      {enableInput ? (
+        <div className="p-4">
+          <ProfileSelectCell
+            type="draftProfile"
+            valueProp="id"
+            labelProp="fullName"
+            excludeIds={profiles.map((p) => p.profile.id)}
+            onSelectionChange={onSelectionChange}
+          />
+        </div>
+      ) : null}
+      <span className="flex-1" />
+      <div className="flex flex-row justify-between px-6 pt-4 pb-2 bg-orange-200">
         <Link
           to={routes.containers({ id })}
           className="inline-block bg-green-500 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
@@ -32,6 +105,14 @@ const ContainerCard = ({ data }) => {
         >
           Sửa
         </Link>
+        <button className="rw-button" onClick={toggleInput}>
+          {enableInput ? 'Tắt Gán' : 'Gán'}
+        </button>
+        {enableInput ? (
+          <button className="rw-button" onClick={attachProfile}>
+            Lưu
+          </button>
+        ) : null}
       </div>
     </div>
   )
--- web/src/components/ProfileListItem/ProfileListItem.js
+++ web/src/components/ProfileListItem/ProfileListItem.js
@@ -1,8 +1,17 @@
+import { useState } from 'react'
 import { Link, routes } from '@redwoodjs/router'
+import classNames from 'classnames'
+
 const ProfileListItem = ({ profile, index }) => {
   const meta = JSON.parse(profile.metaByKeys)
+  const [showButtons, setShowButtons] = useState(false)
+
   return (
-    <div className="flex flex-row justify-between p-4 hover:bg-yellow-500">
+    <div
+      onMouseEnter={() => setShowButtons(true)}
+      onMouseLeave={() => setShowButtons(false)}
+      className="flex flex-row justify-between p-4 hover:bg-yellow-500 h-16"
+    >
       <Link
         to={routes.draftProfile({ id: profile.id })}
         title={`Click để xem thông tin hồ sơ`}
@@ -10,7 +19,14 @@ const ProfileListItem = ({ profile, index }) => {
       >
         {index + 1}. {profile.fullName}
       </Link>
-      <span className="font-semibold text-sm">Nhóm {meta.group}</span>
+      {!showButtons && (
+        <span className="font-semibold text-sm">Nhóm {meta.group}</span>
+      )}
+      <button
+        className={classNames('rw-button', showButtons ? 'visible' : 'hidden')}
+      >
+        Gỡ bỏ
+      </button>
     </div>
   )
 }
